964613c0c7f7e23cb35f3f55011204d2aafe3954
apaskauskyte <apaskauskyte@gmail.com>
2023-05-07 19:07:21 +0300

    Complete ActivityLifeCycles exercise

--- app/src/main/java/lt/ausra/android_topics/ActivityLifeCycles.kt
+++ app/src/main/java/lt/ausra/android_topics/ActivityLifeCycles.kt
@@ -1,4 +1,48 @@
 package lt.ausra.android_topics
 
-class ActivityLifeCycles {
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+
+open class ActivityLifeCycles : AppCompatActivity() {
+
+    fun timber(text: String) {
+        Timber.i(
+            """
+            ********************
+            * ${this.localClassName}
+            * $text
+            ********************
+        """.trimIndent()
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        timber("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        timber("onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timber("onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timber("onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timber("onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        timber("onRestart")
+    }
 }
--- app/src/main/java/lt/ausra/android_topics/MainActivity.kt
+++ app/src/main/java/lt/ausra/android_topics/MainActivity.kt
@@ -7,59 +7,19 @@ import android.widget.Button
 import android.widget.TextView
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityLifeCycles() {
 
     lateinit var openButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timber("onCreate")
 
         openButton = findViewById(R.id.openButton)
-        timber("onCreate")
 
         openButton.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
         }
-    }
-    override fun onStart() {
-        super.onStart()
-        timber("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        timber("onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        timber("onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        timber("onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timber("onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        timber("onRestart")
-    }
-
-    fun timber(text: String) {
-        Timber.i(
-            """
-            ********************
-            * ${this.localClassName}
-            * $text
-            ********************
-        """.trimIndent()
-        )
     }
 }
