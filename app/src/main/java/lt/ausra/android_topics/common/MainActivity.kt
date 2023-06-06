287aa4915980a01b3ddce3216f9fbd01cb795456
apaskauskyte <a.paskauskyte@gmail.com>
2023-06-06 15:01:53 +0300

    Relocate MainActivity

--- app/src/main/java/lt/ausra/android_topics/common/MainActivity.kt
+++ app/src/main/java/lt/ausra/android_topics/common/MainActivity.kt
@@ -1,7 +1,8 @@
-package lt.ausra.android_topics
+package lt.ausra.android_topics.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import lt.ausra.android_topics.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
