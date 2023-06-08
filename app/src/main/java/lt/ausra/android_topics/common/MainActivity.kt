82f277d399fd88a91a81e8bb003f944540ebf761
apaskauskyte <a.paskauskyte@gmail.com>
2023-06-08 14:27:29 +0300

    Use parentFragmentManager to open SecondFragment and implement viewBinding

--- app/src/main/java/lt/ausra/android_topics/common/MainActivity.kt
+++ app/src/main/java/lt/ausra/android_topics/common/MainActivity.kt
@@ -1,11 +1,26 @@
 package lt.ausra.android_topics.common
 
 import android.os.Bundle
+import androidx.fragment.app.commit
 import lt.ausra.android_topics.R
+import lt.ausra.android_topics.first_fragment.FirstFragment
 
 class MainActivity : ActivityLifecyclesPresentation() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openFragment()
+    }
+
+    private fun openFragment() {
+        supportFragmentManager.commit {
+            replace(
+                R.id.fragmentContainerView,
+                FirstFragment.newInstance(),
+                "first_fragment"
+            )
+            setReorderingAllowed(true)
+        }
     }
 }
--- app/src/main/java/lt/ausra/android_topics/first_fragment/FirstFragment.kt
+++ app/src/main/java/lt/ausra/android_topics/first_fragment/FirstFragment.kt
@@ -4,19 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import lt.ausra.android_topics.R
 import lt.ausra.android_topics.common.FragmentLifecyclesPresentation
+import lt.ausra.android_topics.databinding.FragmentFirstBinding
+import lt.ausra.android_topics.second_fragment.SecondFragment
 
 class FirstFragment : FragmentLifecyclesPresentation() {
 
     private val viewModel: FirstFragmentViewModel by viewModels()
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onCLickOpenButton()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onCLickOpenButton() {
+        binding.openSecondFragmentButton.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(
+                    R.id.fragmentContainerView,
+                    SecondFragment.newInstance(),
+                    "second_fragment"
+                )
+                setReorderingAllowed(true)
+            }
+        }
     }
 
     companion object {
